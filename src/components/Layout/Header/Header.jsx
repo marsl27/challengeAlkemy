f81b692af0d4882a02bc3e2047fe7a995d53d64a
Mariel Romeo <marielceciliaromeo@hotmail.com>
2021-12-16 22:14:26 -0300

    [ADD] logo mas h1 que redirige al home y css al formlogin

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,8 +1,16 @@
 import { Link } from "react-router-dom"
 import "./Header.css"
+import icon from "./iconoEscudo.png"
 
-export default function Header(){
-    return(
-        <div className="header">Header <Link to="/login">Login</Link></div>
+export default function Header() {
+    return (
+        <div className="header">
+            
+            <Link to="/"className="containerLogo">
+                <img src={icon} />
+                <h1>SuperTeam</h1>
+            </Link>
+            <button className="btn btn-outline-primary"> <Link to="/login">Login</Link></button>
+        </div>
     )
 }

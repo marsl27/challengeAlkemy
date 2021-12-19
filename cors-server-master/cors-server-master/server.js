b3611ba32ded7284f55f60b0a493588e445a0873
Mariel Romeo <marielceciliaromeo@hotmail.com>
2021-12-19 20:36:57 -0300

    [ADD]cors anywhere local

--- cors-server-master/cors-server-master/server.js
+++ cors-server-master/cors-server-master/server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', 'https://portfolio-ravenous.herokuapp.com'],
+    originWhitelist: ['http://localhost:3000', "https://marsl27.github.io/challengeAlkemy/", 'http://superheroapi.com/api/10227290079499431/'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {

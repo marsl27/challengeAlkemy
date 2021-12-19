7bd75726965b989863f93555e2e5807a1ae0c469
Mariel Romeo <marielceciliaromeo@hotmail.com>
2021-12-19 20:45:53 -0300

    [ADD] url de github a los cors anywhere

--- cors-server-master/cors-server-master/server.js
+++ cors-server-master/cors-server-master/server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:3000', "https://marsl27.github.io/challengeAlkemy/", 'http://superheroapi.com/api/10227290079499431/'],
+    originWhitelist: ['https://marsl27.github.io/challengeAlkemy/', 'https://portfolio-ravenous.herokuapp.com'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {

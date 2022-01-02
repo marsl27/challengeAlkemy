470e32aad1a133a0ea94f06f5d5c1cfb106341fb
Mariel Romeo <marielceciliaromeo@hotmail.com>
2022-01-02 16:33:16 -0300

    harcodee las credenciales para que funcione en github pages

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,7 +5,10 @@ import Form from './Form';
 import axios from "axios"
 import Swal from 'sweetalert2'
 
-
+const credentials = {
+    email: "challenge@alkemy.org",
+    password: "react"
+}
 const validation = values => {
     let errors = {};
 
@@ -38,15 +41,25 @@ const Login = () => (
                     sessionStorage.setItem("log", "true")
                     sessionStorage.setItem("token", response.data.token);
                     sessionStorage.setItem("email", values.email);
-                    window.location.pathname="/challengeAlkemy"
+                    window.location.pathname = "/challengeAlkemy"
                 })
                 .catch(error => {
                     console.log(error);
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Oops...',
-                        text: 'Something went wrong!',
-                      })
+
+                    //Hardcodee las credenciales para que funcione con github pages ya que no funcionaba por error de mixed content debido a que la url de la app es https y el endpoint
+                    //que entrega el token es http
+                    if (values.mail === credentials.email && values.password === credentials.password) {
+                        sessionStorage.setItem("log", "true")
+                        sessionStorage.setItem("token", "");
+                        sessionStorage.setItem("email", values.email);
+                        window.location.pathname = "/challengeAlkemy"
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Something went wrong!',
+                        })
+                    }
                 })
 
         }}
